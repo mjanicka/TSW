ea9687721b5d3f616ee41ede014da3504bc5ec45
mjanicka <m.janickaa@gmail.com>
2013-05-31 21:30:41 +0200

    uporzadkowanie kodu, dodanie opcji wyboru koloru oraz figury, wysylanie wiadomosci do serwera

--- serwer.js
+++ serwer.js
@@ -1,28 +1,35 @@
 var fs = require('fs'),
     http = require('http'),
     io = require('socket.io'),
-    url = require('url'), 
-server = http.createServer(function(req,res){
-	var request = url.parse(req.url, true);
-	var action = request.pathname;
-	
-	if (action == '/') {
-		var login = fs.readFileSync('./logowanie.html');
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(login);
-		res.end();
-	}
-	if (action == '/login') {
-		var login = fs.readFileSync('./logowanie.html');
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(login);
-		res.end();
-	}
-	if (action == '/register') {
-		var login = fs.readFileSync('./logowanie.html');
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(login);
-		res.end();
-	}
-});
-server.listen(8090);
+    url = require('url'),
+
+    server = http.createServer(function (req, res) {
+            var request = url.parse(req.url, true);
+            var action = request.pathname;
+
+            if (action == '/') {
+                var login = fs.readFileSync('./logowanie.html');
+                res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                res.write(login);
+                res.end();
+            }
+            if (action == '/login') {
+                var login = fs.readFileSync('./polerysuj.html');
+                res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                res.write(login);
+                res.end();
+            }
+        });
+server.listen(8080);
+
+var socket = io.listen(server);
+socket.on('connection', function (client) {
+        console.log('connected');
+        client.on('message', function (msg) {
+                console.log(msg);
+            });
+    });
