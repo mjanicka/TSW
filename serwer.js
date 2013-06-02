459330489459064a4fe1d6ea24af99986c5385a8
mjanicka <m.janickaa@gmail.com>
2013-06-02 11:04:03 +0200

    zmiana serwera na express, rejestracja, logowanie i wylogowywanie uzytkownikow, blokada pola wiadomosci

--- serwer.js
+++ serwer.js
@@ -2,57 +2,101 @@ var fs = require('fs'),
     http = require('http'),
     io = require('socket.io'),
     url = require('url'),
+    express = require('express'),
 
-    server = http.createServer(function (req, res) {
-            var request = url.parse(req.url, true);
-            var action = request.pathname;
+    server = express();
 
-            if (action == '/') {
-                var strona = fs.readFileSync('./logowanie.html');
-                res.writeHead(200, {
-                        'Content-Type': 'text/html'
-                    });
-                res.write(strona);
-                res.end();
-            }
-            if (action == '/login') {
-				if( req.session ){
-					console.log( req.session );
-				}				
-				var post;
-                req.on('data', function (data) {
-					var tmp = "" + data;
-					post = require( "querystring" ).parse(tmp);
-					
-					var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
-					var wynik = baza.uzytkownicy.find( { login: post.login }, function( err, user ){
-						if( user[0] ){
-							if(user[0].haslo == post.haslo) {
-								req.session.user = user[0].login;
-								var strona = fs.readFileSync('./polerysuj.html');
-								res.writeHead(200, {
-								'Content-Type': 'text/html'
-								});
-								res.write(strona);
-								res.send();
-							}
-						}
-						var strona = fs.readFileSync('./logowanie.html');
-						res.writeHead(200, {
-						'Content-Type': 'text/html; charset=utf-8'
-						});	
-						res.write("<p style=\"color:red\">zły login bądź hasło</p>");
-						res.write(strona);
-						res.end();
-					});
-				});
-                
-                
-            }
+server.configure(function(){
+	server.use(express.cookieParser()); 
+	server.use(express.bodyParser());
+	server.use( express.session( { secret: 'xxx' } ) );
+});
+
+server.get('/', function( req, res ){
+ 	console.log( "session: " + req.session.login );
+        var strona = fs.readFileSync('./logowanie.html');
+        res.writeHead(200, {
+	        'Content-Type': 'text/html'
         });
+        res.write(strona);
+        res.send();
+});
+
+server.post( '/login', function( req, res ){
+ 	console.log( "session: " + req.session.login );
+ 	var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
+ 	baza.uzytkownicy.find({login:req.body.login}, function(err, user) {
+		if (user[0] &&  user[0].haslo == req.body.haslo) {
+			var strona = fs.readFileSync('./polerysuj.html');
+			req.session.login = req.body.login;
+			res.writeHead(200, {
+				'Content-Type': 'text/html; charset=utf-8'	
+			});
+			res.write(strona);
+			res.send();
+		}
+		else {
+			var strona = fs.readFileSync('./logowanie.html');
+			res.writeHead(200, {
+				'Content-Type': 'text/html; charset=utf-8'	
+			});
+			res.write("błędny login bądź hasło");
+			res.write(strona);
+			res.send();
+		}
+	});
+});
+
+server.get('/logout', function( req, res ){
+	req.session = null;
+    var strona = fs.readFileSync('./logowanie.html'); 
+    res.writeHead(200, {
+	   'Content-Type': 'text/html'
+    });
+    res.write(strona);
+    res.send();
+});
+
+server.post( '/register', function( req, res ){
+ 	var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
+ 	if(req.body.haslorej == req.body.powtorzhaslorej) {
+		baza.uzytkownicy.find({login:req.body.loginrej}, function(err, user) {
+			if(user[0]){
+				var strona = fs.readFileSync('./logowanie.html');
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf-8'	
+				});
+				res.write("użytkownik istnieje już w bazie");
+				res.write(strona);
+				res.send();
+			}
+			else {
+				baza.uzytkownicy.insert({login:req.body.loginrej, haslo:req.body.haslorej});
+				var strona = fs.readFileSync('./polerysuj.html');
+				res.writeHead(200, {
+					'Content-Type': 'text/html; charset=utf-8'	
+				});
+				res.write("użytkownik został dodany");
+				res.write(strona);
+				res.send();
+			}
+		});
+	}
+	else {
+		var strona = fs.readFileSync('./logowanie.html');
+		res.writeHead(200, {
+			'Content-Type': 'text/html; charset=utf-8'	
+		});
+		res.write("hasła się nie zgadzają");
+		res.write(strona);
+		res.send();
+	}
+});
+
 server.listen(8080);
 
-var socket = io.listen(server);
+var	httpserv = http.createServer(server).listen(8081),
+	socket = io.listen(httpserv);
 socket.on('connection', function (client) {
         console.log('connected');
         client.on('message', function (msg) {
