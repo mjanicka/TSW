d1ef90b65c8c1ab0212b9464df0719eaab6dfdbd
mjanicka <m.janickaa@gmail.com>
2013-06-04 18:27:55 +0200

    oddzielenie kodu javascript od html, wyswietlanie prostokatow, linii i kol u drugiego uzytkownika, zmiany za sugestia jshint

---
+++ polerysuj.js
@@ -0,0 +1,212 @@
+"use strict"; 
+			
+var downX;
+var downY;
+var ismousedown = 0;
+var socket = new io.connect('http://localhost:8081');
+
+function getCookie (name) {
+	var result = null;
+	var cookies = document.cookie.split("; ");
+	cookies.forEach(function(cookie) {
+		var tmp = cookie.split("=");
+		if(tmp[0] === name) {
+			result = "" + tmp[1];
+		}
+	});
+	return result;
+
+}
+
+var userid = getCookie("userid");
+
+
+function mousedown(event) {
+	var canvas = document.getElementById("mojCanvas");
+	var context = canvas.getContext('2d');
+	var kolor = document.getElementById("kolor");
+	var figura = document.getElementById("figura");
+	var x = event.clientX;
+	var y = event.clientY;
+
+	x -= canvas.offsetLeft;
+	y -= canvas.offsetTop;
+
+	downX = x;
+	downY = y;
+
+	context.beginPath();
+
+	if (kolor.value) {
+		context.strokeStyle = kolor.value;
+	}
+
+	if (figura.value === "punkt") {
+		context.rect(x, y, 2, 2);
+		context.stroke();
+		socket.emit('punkt',[x,y, context.strokeStyle]);
+	}
+
+	ismousedown = 1;
+}
+
+function mousemove(event) {
+	if (ismousedown === 1) {
+		var canvas = document.getElementById("mojCanvas");
+		var context = canvas.getContext('2d');
+		var figura = document.getElementById("figura");
+		var x = event.clientX;
+		var y = event.clientY;
+
+		x -= canvas.offsetLeft;
+		y -= canvas.offsetTop;
+
+		if (figura.value === "punkt") {
+			context.rect(x, y, 2, 2);
+			context.stroke();
+			socket.emit('punkt',[x,y, context.strokeStyle]);
+		}
+	}
+}
+
+function mouseup(event) {
+	ismousedown = 0;
+
+	var canvas = document.getElementById("mojCanvas");
+	var context = canvas.getContext('2d');
+	var figura = document.getElementById("figura");
+	var x = event.clientX;
+	var y = event.clientY;
+	var radius = x - downX;	
+	x -= canvas.offsetLeft;
+	y -= canvas.offsetTop;
+
+	switch (figura.value) {
+	case "prostokąt":
+		context.rect(downX, downY, x - downX, y - downY);
+		context.stroke();
+		socket.emit('prostokąt',[downX, downY, x - downX, y - downY, context.strokeStyle]);
+	break;
+	
+	case "koło":
+		context.arc(downX, downY, radius, x, y, false);
+		context.stroke();
+		socket.emit('koło',[downX, downY, radius, x, y, context.strokeStyle]);
+	break;
+	
+	case "linia":
+		context.moveTo(downX, downY);
+		context.lineTo(x , y);
+		context.stroke();
+		socket.emit('linia',[downX, downY, x , y, context.strokeStyle]);
+	break;
+	}
+}
+
+function doserwera(text) {
+	socket.send(text);
+}
+
+
+function wyslij() {
+	var tekst = document.getElementById("wiadomosc");
+
+	doserwera( tekst.value );
+	tekst.value = "";
+}
+
+function enter(event) {
+	if (event.keyCode === 13) {
+		wyslij();
+	}
+}
+
+function wyloguj() {
+socket.emit( 'logout', '' );
+window.location = "/logout";
+}
+
+function enableDrawing(){
+var canvas = document.getElementById("mojCanvas");
+	canvas.onmousedown = mousedown;
+	canvas.onmousemove = mousemove;
+	canvas.onmouseup = mouseup;
+}
+
+function disableDrawing(){
+var canvas = document.getElementById("mojCanvas");
+	canvas.onmousedown = null;
+	canvas.onmousemove = null;
+	canvas.onmouseup = null;
+}
+
+function init() {
+	var czat = document.getElementById("czat");
+	var wpisz = document.getElementById("wiadomosc");
+					
+	wpisz.onkeyup = enter;
+	
+	socket.emit( "start-connection", userid );
+	
+	socket.on('message', function (msg) {
+		czat.innerHTML += msg;
+	});
+	
+	socket.on('start-drawing', function(thing){
+		czat.innerHTML += "Zaczynasz grę, narysuj " + thing + "\n";
+		enableDrawing();
+	});
+
+	socket.on('end-game', function(message){
+		czat.innerHTML += message + "\n";
+		disableDrawing();
+		
+		var canvas = document.getElementById("mojCanvas");
+		canvas.width = canvas.width;
+	});
+	
+	socket.on('punkt', function(wspolrzedne) {
+		var canvas = document.getElementById("mojCanvas");
+		var context = canvas.getContext('2d');
+		
+		
+		context.beginPath();
+		context.rect(wspolrzedne[0], wspolrzedne[1], 2, 2);
+		context.strokeStyle = wspolrzedne[2];
+		context.stroke();
+	});
+	
+	socket.on('prostokąt', function(wspolrzedne) {
+		var canvas = document.getElementById("mojCanvas");
+		var context = canvas.getContext('2d');
+		
+		
+		context.beginPath();
+		context.rect(wspolrzedne[0], wspolrzedne[1], wspolrzedne[2], wspolrzedne[3]);
+		context.strokeStyle = wspolrzedne[4];
+		context.stroke();
+	});
+	
+	socket.on('koło', function(wspolrzedne) {
+		var canvas = document.getElementById("mojCanvas");
+		var context = canvas.getContext('2d');
+		
+		context.beginPath();
+		context.arc(wspolrzedne[0], wspolrzedne[1], wspolrzedne[2], wspolrzedne[3], wspolrzedne[4], false);
+		context.strokeStyle = wspolrzedne[5];
+		context.stroke();
+	});
+	
+	socket.on('linia', function(wspolrzedne) {
+		var canvas = document.getElementById("mojCanvas");
+		var context = canvas.getContext('2d');
+		
+		
+		context.beginPath();
+		context.moveTo(wspolrzedne[0], wspolrzedne[1]);
+		context.lineTo(wspolrzedne[2], wspolrzedne[3]);
+		context.strokeStyle = wspolrzedne[4];
+		context.stroke();
+	});
+	 
+}
--- serwer.js
+++ serwer.js
@@ -1,199 +1,237 @@
+"use strict";
+
 var fs = require('fs'),
     http = require('http'),
     io = require('socket.io'),
     express = require('express'),
 
     server = express(),
-    players= new Object(),
+    players = {},
     currentPlayer = null,
     playersCount = 0,
-    currentWord = null;
+    currentWord = null,
+    strona, ids;
 
-function randomString( len ){
-	var result = "";
-	for( var i = 0; i < len; i++ ){
-		var ascii = Math.floor( Math.random() * 93 ) + 33 ; // from '!'(33) to '~'(126)
-		if (ascii == 59 || ascii == 61 ){ // ; and = dont wanted as special chars in many cases
-			ascii = 33;
-		}
-		result += String.fromCharCode( ascii )
-	}
-	return result;
+
+function randomString(len) {
+    var result = "", i, ascii;
+
+    for (i = 0; i < len; i++) {
+        ascii = Math.floor(Math.random() * 93) + 33; // from '!'(33) to '~'(126)
+        if (ascii === 59 || ascii === 61) { // ; and = dont wanted as special chars in many cases
+            ascii = 33;
+        }
+        result += String.fromCharCode(ascii);
+    }
+    return result;
 }
 
-function randomWord(){
-	var slowa = ['pies','kot','ulica', 'chmura', 'ekspedientka', 'samochód', 'zabawki'],
-		slowo = slowa[Math.floor(Math.random() * slowa.length)];
- 	
- 	return slowo;
+function randomWord() {
+    var slowa = ['pies', 'kot', 'ulica', 'chmura', 'ekspedientka', 'samochód', 'zabawki'],
+        slowo = slowa[Math.floor(Math.random() * slowa.length)];
+
+    return slowo;
 }
 
-server.configure(function(){
-	server.use(express.cookieParser()); 
-	server.use(express.bodyParser());
-	server.use( express.session( { secret: randomString(5) } ) );
+server.configure(function() {
+    server.use(express.cookieParser());
+    server.use(express.bodyParser());
+    server.use(express.session({ secret: randomString(5) }));
 });
 
-server.get('/', function( req, res ){
-        var strona = fs.readFileSync('./logowanie.html');
-        res.writeHead(200, {
-	        'Content-Type': 'text/html'
-        });
-        res.write(strona);
-        res.send();
-});
-
-server.post( '/login', function( req, res ){
-	var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
- 	baza.uzytkownicy.find({login:req.body.login}, function(err, user) {
-		if (user[0] &&  user[0].haslo == req.body.haslo) {
-			console.log( "session: " + req.session.login );
-			var strona = fs.readFileSync('./polerysuj.html');
-			req.session.login = req.body.login;
-			var userid = randomString(5);
-			players[userid] = { name: req.body.login };
-			console.log( "created player login: " + req.body.login + " id: " + userid + "\n" );
-
-			res.writeHead(200, {
-				'Content-Type': 'text/html; charset=utf-8',
-				'Set-Cookie': 'userid=' + userid
-				});
-
-			res.write(strona);
-			res.send();
-		}
-		else {
-			var strona = fs.readFileSync('./logowanie.html');
-			res.writeHead(200, {
-				'Content-Type': 'text/html; charset=utf-8'	
-			});
-			res.write("błędny login bądź hasło");
-			res.write(strona);
-			res.send();
-		}
-	});
-});
-
-server.get('/logout', function( req, res ){
-	req.session = null;
-    var strona = fs.readFileSync('./logowanie.html'); 
+server.get('/', function(req, res) {
+    var strona = fs.readFileSync('./logowanie.html');
     res.writeHead(200, {
-	   'Content-Type': 'text/html'
+        'Content-Type': 'text/html'
     });
     res.write(strona);
     res.send();
 });
 
-server.post( '/register', function( req, res ){
- 	var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
- 	if(req.body.haslorej == req.body.powtorzhaslorej) {
-		baza.uzytkownicy.find({login:req.body.loginrej}, function(err, user) {
-			if(user[0]){
-				var strona = fs.readFileSync('./logowanie.html');
-				res.writeHead(200, {
-					'Content-Type': 'text/html; charset=utf-8'	
-				});
-				res.write("użytkownik istnieje już w bazie");
-				res.write(strona);
-				res.send();
-			}
-			else {
-				baza.uzytkownicy.insert({login:req.body.loginrej, haslo:req.body.haslorej});
-				var strona = fs.readFileSync('./polerysuj.html');
-				var userid = randomString(5);
-				players[userid] = {name: req.body.loginrej};
-				res.writeHead(200, {
-					'Content-Type': 'text/html; charset=utf-8',	
-					'Set-Cookie': 'userid=' + userid
-				});
-				res.write("użytkownik został dodany");
-				res.write(strona);
-				res.send();
-			}
-		});
-	}
-	else {
-		var strona = fs.readFileSync('./logowanie.html');
-		res.writeHead(200, {
-			'Content-Type': 'text/html; charset=utf-8'	
-		});
-		res.write("hasła się nie zgadzają");
-		res.write(strona);
-		res.send();
-	}
+server.get('/polerysuj.js', function(req, res) {
+    var strona = fs.readFileSync('./polerysuj.js');
+    res.writeHead(200, {
+        'Content-Type': 'text/javascript'
+    });
+    res.write(strona);
+    res.send();
+});
+
+server.post('/login', function(req, res) {
+    var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
+     baza.uzytkownicy.find({login:req.body.login}, function(err, user) {
+        if (user[0] &&  user[0].haslo === req.body.haslo) {
+            console.log("session: " + req.session.login);
+            strona = fs.readFileSync('./polerysuj.html');
+            req.session.login = req.body.login;
+            var userid = randomString(5);
+            players[userid] = { name: req.body.login };
+            console.log("created player login: " + req.body.login + " id: " + userid + "\n");
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8',
+                'Set-Cookie': 'userid=' + userid
+                });
+
+            res.write(strona);
+            res.send();
+        }
+        else {
+            strona = fs.readFileSync('./logowanie.html');
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'    
+            });
+            res.write("błędny login bądź hasło");
+            res.write(strona);
+            res.send();
+        }
+    });
+});
+
+server.get('/logout', function(req, res) {
+    req.session = null;
+    var strona = fs.readFileSync('./logowanie.html'); 
+    res.writeHead(200, {
+       'Content-Type': 'text/html'
+    });
+    res.write(strona);
+    res.send();
+});
+
+server.post('/register', function(req, res) {
+     var baza = require("mongojs").connect("kalambury",["uzytkownicy"]);
+     if(req.body.haslorej === req.body.powtorzhaslorej) {
+        baza.uzytkownicy.find({login:req.body.loginrej}, function(err, user) {
+            if(user[0]) {
+                strona = fs.readFileSync('./logowanie.html');
+                res.writeHead(200, {
+                    'Content-Type': 'text/html; charset=utf-8'    
+                });
+                res.write("użytkownik istnieje już w bazie");
+                res.write(strona);
+                res.send();
+            }
+            else {
+                baza.uzytkownicy.insert({login:req.body.loginrej, haslo:req.body.haslorej});
+                strona = fs.readFileSync('./polerysuj.html');
+                var userid = randomString(5);
+                players[userid] = {name: req.body.loginrej};
+                res.writeHead(200, {
+                    'Content-Type': 'text/html; charset=utf-8',    
+                    'Set-Cookie': 'userid=' + userid
+                });
+                res.write("użytkownik został dodany");
+                res.write(strona);
+                res.send();
+            }
+        });
+    }
+    else {
+        strona = fs.readFileSync('./logowanie.html');
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'    
+        });
+        res.write("hasła się nie zgadzają");
+        res.write(strona);
+        res.send();
+    }
 });
 
 server.listen(8080);
 
-var	httpserv = http.createServer(server).listen(8081),
-	socket = io.listen(httpserv);
+var    httpserv = http.createServer(server).listen(8081),
+    socket = io.listen(httpserv);
 
 socket.on('connection', function (client) {
-	var clientUID = null;
-	
-	function startNewGame(){
-		var playerNum = Math.floor( Math.random() * playersCount );
-		var playerIds = Object.keys( players );
-		currentPlayer = playerIds[playerNum];
-		playerIds.forEach( function(pid){
-			console.log ( "test: " + pid );
-			if( pid == currentPlayer ){
-				currentWord = randomWord();
-				players[pid]["sock"].emit( "start-drawing", currentWord );
-			} else{
-				players[pid]["sock"].emit( "message", players[currentPlayer]["name"] +
-					" zaczyna rysować.\n" );
-			}
-		});
-	}
-		
+    var clientUID = null;
+    
+    function startNewGame() {
+        var playerNum = Math.floor(Math.random() * playersCount);
+        var playerIds = Object.keys(players);
+        currentPlayer = playerIds[playerNum];
+        playerIds.forEach(function(pid) {
+            console.log ("test: " + pid);
+            if(pid === currentPlayer) {
+                currentWord = randomWord();
+                players[pid].sock.emit("start-drawing", currentWord);
+            } else{
+                players[pid].sock.emit("message", players[currentPlayer].name +
+                    " zaczyna rysować.\n");
+            }
+        });
+    }
+        
     client.on('start-connection', function (userid) {
-		clientUID = userid;
-		players[userid]["sock"] = client;
-		client.emit( "message", "Witaj " + players[userid]["name"] + ".\n" );
-		playersCount ++;
-		
-		if( currentPlayer == null && playersCount > 1 ){
-			startNewGame();
-		}
-	});
+        clientUID = userid;
+        players[userid].sock = client;
+        client.emit("message", "Witaj " + players[userid].name + ".\n");
+        playersCount ++;
+        
+        if(currentPlayer === null && playersCount > 1) {
+            startNewGame();
+        }
+    });
 
-	client.on( 'message', function(text){
-		if(text == currentWord){
-			var ids = Object.keys(players);
-			ids.forEach(function(id){
-				players[id]["sock"].emit("end-game", "gracz " + players[id]["name"] + " odgadł hasło: " + currentWord);
-			});
-			startNewGame();
-		}
-		else {
-			var ids = Object.keys(players);
-			ids.forEach(function(id){
-				players[id]["sock"].emit('message', players[clientUID]["name"] + ": " + text + "\n");
-			});
-		}
-	});
-	
-	client.on('punkt', function(wspolrzedne){
-		Object.keys(players).forEach(function(id) {
-			if(currentPlayer != id){
-				players[id]['sock'].emit('punkt', wspolrzedne);
-			}
-		});
-	});
-	
-	client.on('logout', function(){
-		delete players[clientUID];
+    client.on('message', function(text) {
+        if(text === currentWord) {
+            ids = Object.keys(players);
+            ids.forEach(function(id) {
+                players[id].sock.emit("end-game", "gracz " + players[id].name + " odgadł hasło: " + currentWord);
+            });
+            startNewGame();
+        }
+        else {
+            ids = Object.keys(players);
+            ids.forEach(function(id) {
+                players[id].sock.emit('message', players[clientUID].name + ": " + text + "\n");
+            });
+        }
+    });
+    
+    client.on('punkt', function(wspolrzedne) {
+        Object.keys(players).forEach(function(id) {
+            if(currentPlayer !== id) {
+                players[id].sock.emit('punkt', wspolrzedne);
+            }
+        });
+    });
+    
+    client.on('prostokąt', function(wspolrzedne) {
+        Object.keys(players).forEach(function(id) {
+            if(currentPlayer !== id) {
+                players[id].sock.emit('prostokąt', wspolrzedne);
+            }
+        });
+    });
+    
+    client.on('koło', function(wspolrzedne) {
+        Object.keys(players).forEach(function(id) {
+            if(currentPlayer !== id) {
+                players[id].sock.emit('koło', wspolrzedne);
+            }
+        });
+    });
+    
+    client.on('linia', function(wspolrzedne) {
+        Object.keys(players).forEach(function(id) {
+            if(currentPlayer !== id) {
+                players[id].sock.emit('linia', wspolrzedne);
+            }
+        });
+    });
+    
+    client.on('logout', function() {
+        delete players[clientUID];
 
-		if( --playersCount == 1 ){
-			players[Object.keys(players)[0]]["sock"].emit( "end-game", "Pozostali użytkownicy opuścili grę, gra zostaje przerwana." );
-			currentPlayer = null;
-		} else if( clientUID == currentPlayer ){
-			Object.keys(players).forEach( function( playerID ){
-				players[playerID]["sock"].emit( "end-game", "Rysujący opuścił grę, gra zostaje przerwana." );
-				currentPlayer = null;
-			});
-			startNewGame();
-		}
-	});
+        if(--playersCount === 1) {
+            players[Object.keys(players)[0]].sock.emit("end-game", "Pozostali użytkownicy opuścili grę, gra zostaje przerwana.");
+            currentPlayer = null;
+        } else if(clientUID === currentPlayer) {
+            Object.keys(players).forEach(function(playerID) {
+                players[playerID].sock.emit("end-game", "Rysujący opuścił grę, gra zostaje przerwana.");
+                currentPlayer = null;
+            });
+            startNewGame();
+        }
+    });
  });
